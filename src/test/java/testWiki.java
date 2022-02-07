b86e70f1b17a3be090bb01c685484b0ee6cab388
KostrulinNS <max90003@gmail.com>
2022-02-07 17:25:51 +0300

    editing

--- src/test/java/testWiki.java
+++ src/test/java/testWiki.java
@@ -21,16 +21,19 @@ public class testWiki {
     @BeforeEach
     void initDriver(){
         driver = new ChromeDriver();
+        actions = new Actions(driver);
         webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(5));
         driver.get(WIKI_URL);
     }
     @Test
     void hoverMenuTest() throws InterruptedException {
         Thread.sleep(5000);
-        actions.moveToElement(driver.findElement(By.xpath("//a[.='Случайная статья']"))).click()
+        actions.moveToElement(driver.findElement(By.xpath("//a[.='Избранные статьи']"))).click()
                 .build()
                 .perform();
-        Assertions.assertTrue(driver.getCurrentUrl().contains("Обсуждение_участника"));
+        Assertions.assertTrue(driver.getCurrentUrl().contains("https://ru.wikipedia.org/wiki/%D0%92%D0%B8%D0%BA%D0%B8%D" +
+                "%BF%D0%B5%D0%B4%D0%B8%D1%8F:%D0%98%D0%B7%D0%B1%D1%80%D0%B0%D0%BD%D0%BD%D1%8B%D0%B5_%D1%81%D1%82%D0%B0%D" +
+                "1%82%D1%8C%D0%B8"));
     }
     @AfterEach
     void tearDown(){
